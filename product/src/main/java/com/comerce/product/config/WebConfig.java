b6cb53ffd7bfc63d75a8fe5e048b0bf6eba204a2
PedroBarge <pedro525tavares@gmail.com>
2024-07-26 23:44:49 +0100

    fix CORS

--- product/src/main/java/com/comerce/product/config/WebConfig.java
+++ product/src/main/java/com/comerce/product/config/WebConfig.java
@@ -10,9 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                //.allowedOrigins("*")
+                .allowedOrigins()
                 .allowedMethods("GET","POST","PUT","DELETE")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(true)
+        ;
     }
 }
